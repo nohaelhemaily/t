cad6895b62cd83ea52cdb3d1d629d94e77a87205
Noha <noha.elhemaily11@icloud.com>
2024-12-27 18:34:26 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+const stringToNumber = function(str){
+ 
+  
+    return Number(str);
+  }
+     console.log(stringToNumber("1234"));
+     console.log(stringToNumber("605"));
+     console.log(stringToNumber("1405"));
+     console.log(stringToNumber("-7"));
+  
